b5681a3c85910c9e5e9235c6d4ca0fa54f659785
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2013-07-16 14:50:39 +0300

    Updated kernel patching tool, updated grsecurity version

--- custom-packages/linux-image/setup.sh
+++ custom-packages/linux-image/setup.sh
@@ -12,6 +12,8 @@ KERNEL_URL="https://www.kernel.org/pub/linux/kernel/v3.x/linux-${KERNEL_VERSION}
 GRSECURITY_URL="https://grsecurity.net/stable/grsecurity-${GRSECURITY_VERSION}.patch"
 AUFS_URL="http://git.code.sf.net/p/aufs/aufs3-standalone"
 
+KERNEL_DIR="linux-${KERNEL_VERSION}"
+KERNEL_ARCHIVE="${KERNEL_DIR}.tar.xz"
 AUFS_CHECKOUT_PATH="aufs3-standalone"
 
 WGET_BIN="/usr/bin/wget"
@@ -25,15 +27,32 @@ GIT_FLAGS="-q"
 TAR_FLAGS="-xJf"
 PATCH_FLAGS="-s -p1"
 
-echo "Get kernel source..."
-${WGET_BIN} ${WGET_FLAGS} "${KERNEL_URL}"
+if [ ! -f "${KERNEL_ARCHIVE}" ]
+then
+	echo "Get kernel source..."
+	${WGET_BIN} ${WGET_FLAGS} "${KERNEL_URL}"
+else
+	echo "Kernel source already downloaded..."
+	echo "TODO: check kernel source checksum, verify signature"
+fi
+
+if [ ! -d "${KERNEL_DIR}" ]
+then
+	echo "Extracting kernel source..."
+	${TAR_BIN} ${TAR_FLAGS} "${KERNEL_ARCHIVE}"
+else
+	echo "Kernel source already extracted..."
+	echo "TODO: Check if source has been already patched"
+fi
+
+echo "TODO: check if patch already downloaded / applied"
 echo "Get grsecurity patch..."
 ${WGET_BIN} ${WGET_FLAGS} "${GRSECURITY_URL}"
+
+echo "TODO: Check if repository already cloned, pull newest changes"
 echo "Get AUFS source..."
 ${GIT_BIN} clone ${GIT_FLAGS} "${AUFS_URL}" "${AUFS_CHECKOUT_PATH}"
 
-echo "Extract kernel source..."
-${TAR_BIN} ${TAR_FLAGS} "linux-${KERNEL_VERSION}.tar.xz"
 
 echo "Checkout correct AUFS branch..."
 cd "${AUFS_CHECKOUT_PATH}"
@@ -54,4 +73,9 @@ ${PATCH_BIN} ${PATCH_FLAGS} <"../${AUFS_CHECKOUT_PATH}/aufs3-standalone.patch"
 echo "Copy required files from AUFS source tree..."
 ${CP_BIN} -R "../${AUFS_CHECKOUT_PATH}/fs" "../${AUFS_CHECKOUT_PATH}/Documentation" .
 ${CP_BIN} "../${AUFS_CHECKOUT_PATH}/include/linux/aufs_type.h" ./include/linux/
+
+echo "TODO: Check if patching succeeds"
+echo "TODO: Fix these:"
+echo " 1 out of 2 hunks FAILED -- saving rejects to file fs/proc/task_mmu.c.rej"
+echo " 1 out of 1 hunk FAILED -- saving rejects to file kernel/fork.c.rej"
 
