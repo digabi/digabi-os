20824f120f5d6f35ddbd2314f75240ad25ff9f2f
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2014-04-25 02:42:50 +0300

    Allow using local repository config when initializing Vagrantbox.

--- scripts/vagrant-init.sh
+++ scripts/vagrant-init.sh
@@ -5,6 +5,7 @@ set -e
 #
 # Initiliaze new Vagrant (Debian wheezy/jessie)
 #
+DIGABI_REPOSITORY_PATH="/vagrant/custom-packages/digabi-repository"
 
 if [ -f "/etc/apt/apt.conf" ]
 then
@@ -13,8 +14,18 @@ then
 fi
 
 echo "I: Add Digabi repository..."
-wget -qO /etc/apt/sources.list.d/digabi.list https://digabi.fi/debian/digabi.list
-wget -qO- https://digabi.fi/debian/digabi.asc | apt-key add -
+if [ -f "${DIGABI_REPOSITORY_PATH}/digabi.list" ]
+then
+    cp ${DIGABI_REPOSITORY_PATH}/digabi-list /etc/apt/sources.list.d/digabi.list
+else
+    wget -qO /etc/apt/sources.list.d/digabi.list https://digabi.fi/debian/digabi.list
+fi
+if [ -f "${DIGABI_REPOSITORY_PATH}/digabi.asc" ]
+then
+    apt-key add ${DIGABI_REPOSITORY_PATH}/digabi.asc
+else
+    wget -qO- https://digabi.fi/debian/digabi.asc | apt-key add -
+fi
 
 echo "I: Install digabi-dev..."
 apt-get -qy update
