2cd11479f705d81a90224c0fd0111fa22437c995
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2014-07-04 16:15:34 +0300

    Updating jenkins build script for Tails.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -2,17 +2,23 @@
 
 set -ex
 
+REPOSITORY_HOST="renki.local"
+PROXY_HOST="ci.local"
+CPUS="4"
+
 cleanup() {
-    vagrant destroy -f
+#    rake vm:halt
+    rake vm:destroy
 }
 
 mdns_resolve() {
-
+    # Linux
     if which avahi-resolve > /dev/null 2>&1 ; then
         avahi-resolve -4 -n "$1" | cut -f2
         return
     fi
 
+    # Mac OS X
     if which dscacheutil > /dev/null 2>&1 ; then
         dscacheutil -q host -a name "$1" | awk '/ip_addr/ {print $2; exit}'
         return
@@ -22,30 +28,15 @@ mdns_resolve() {
     exit 42
 }
 
-renki_ip=`mdns_resolve renki.local`
-ci_ip=`mdns_resolve ci.local`
 
-proxy="http://${ci_ip}:3128"
+REPOSITORY_IP="$(mdns_resolve ${REPOSITORY_HOST})"
+PROXY_IP="$(mdns_resolve ${PROXY_HOST})"
 
-for f in Vagrantfile Makefile scripts/vagrant-init.sh ; do
-    sed -i.bak -e "s/ci.local/${ci_ip}/g" -e "s/renki.local/${renki_ip}/g" $f
-    rm $f.bak
-done
+PROXY="http://${PROXY_IP}:3128/"
 
-if which lscpu > /dev/null 2>&1 ; then
-    cpus=$(lscpu |awk '/^CPU\(s\)/ {print $2}')
-    if [ $cpus -ge 8 ] ; then
-        sed -i.bak -e '/"--cpus"/s/"[0-9][0-9]*"/"4"/' -e '/"--memory"/s/"[0-9][0-9]*"/"4096"/' Vagrantfile
-    fi
-fi
-
-rm -rf dist
-vagrant plugin list | grep -q proxyconf || vagrant plugin install vagrant-proxyconf
-vagrant up --provision
 trap cleanup EXIT
 
-[ -n "${renki_ip}" ]
+# TODO: Use specified repository
 
-echo "HTTP_PROXY=${proxy}/ HTTPS_PROXY=${proxy}/ http_proxy=${proxy}/ https_proxy=${proxy}/" > config/environment.chroot
-
-vagrant ssh -c "sudo rsync -Pav --delete /vagrant/ /home/vagrant/build-live/ && REPOSITORY=http://${renki_ip}/debian/ make -C /home/vagrant/build-live/ dist && rsync -Pav --delete /home/vagrant/build-live/dist/ /vagrant/dist/"
+# Build image
+http_proxy="${PROXY}" DIGABI_BUILD_OPTIONS="noram cpus=${CPUS}" rake build ; rake vm:halt
