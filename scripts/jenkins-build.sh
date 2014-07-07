32a0169578f8c0435e42c84dee6b74b5d034e2a7
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2014-07-07 11:45:54 +0300

    Use custom Debian mirror.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -39,4 +39,4 @@ trap cleanup EXIT
 # TODO: Use specified repository when building image
 
 # Build image
-http_proxy="${PROXY}" DIGABI_BUILD_OPTIONS="cpus=${CPUS} ignorechanges noram" rake build
+DEBIAN_MIRROR="http://${REPOSITORY_IP}/debian" http_proxy="${PROXY}" DIGABI_BUILD_OPTIONS="cpus=${CPUS} ignorechanges noram" rake build
