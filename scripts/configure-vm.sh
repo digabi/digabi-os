c014a447d1ec7eb8ab3315cfeb489afa70178af7
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-06-24 09:22:31 +0300

    Add script for configuring build environment in VM.

---
+++ scripts/configure-vm.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+CONFIG="/vagrant/dos-config"
+
+[ -f "${CONFIG}" ] && . ${CONFIG} && echo "I: Using local configuration"
+
+BUILD_DIR="${HOME}/digabi-os"
+
+if [ ! -d "${BUILD_DIR}" ]
+then
+    echo "I: Clone git repository..."
+    git clone /digabi-os.git ${BUILD_DIR}
+fi
+
+echo "I: Checkout git repository..."
+cd ${BUILD_DIR}
+git checkout ${COMMIT:-HEAD}
+
+echo "I: Initialize submodules..."
+git submodule init
+git submodule update
+
+echo "I: Copy local configuration to build directory..."
+cp ${CONFIG} ${BUILD_DIR}/target/default/digabi.local
+
+echo "I: Running lb config..."
+lb config ${LB_OPTIONS}
+
