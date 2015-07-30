8940ea3812118ea99cc139f3a15bdc8618360dfb
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-07-30 10:58:12 +0300

    Refactor firewall generator script.

--- custom-packages/digabios-customization/firewall/digabios-firewall.sh
+++ custom-packages/digabios-customization/firewall/digabios-firewall.sh
@@ -9,6 +9,11 @@
 
 set -e
 
+CONFDIR="/etc/digabi/firewall.d"
+[ -r /etc/default/digabios-firewall ] && . /etc/default/digabios-firewall
+
+[ ! -d "${CONFDIR}" ] && echo "E: Config directory not found!" && exit 1
+
 if [ "$(id -u)" != 0 ]
 then
     echo "E: Script must be run as root."
@@ -22,36 +27,17 @@ then
     COMMAND="compile"
 fi
 
-CONFIG_DIR="/etc/digabi/firewall.d"
-RULEFILE="/etc/iptables/rules.v4"
+VERSIONS="v4 v6"
 
-FILES="$(ls ${CONFIG_DIR}/*.conf)"
+TEMPDIR="$(mktemp -d)"
 
-case "${COMMAND}" in
-    compile)
-        TEMPFILE="$(mktemp)"
-        for f in ${FILES}
-        do
-            echo "I: Adding firewall configuration: ${f}..." >&2
-            if [ -x "${f}" ]
-            then
-                ${f} >>${TEMPFILE}
-            else
-                cat "${f}" >>${TEMPFILE}
-            fi
-        done
+for version in ${VERSIONS}
+do
+    TEMPFILE="${TEMPDIR}/rules.${version}"
+    TARGETFILE="/etc/iptables/rules.${version}"
+    CONFFILES="$(find ${CONFDIR} -maxdepth 1 -type f -name *.${version}.conf)"
 
-        if [ "${2}" = "save" ]
-        then
-            mv "${TEMPFILE}" "${RULEFILE}"
-            chmod 0700 "${RULEFILE}"
-            chown root:root "${RULEFILE}"
-        else
-            cat "${TEMPFILE}"
-        fi
-    ;;
-    *)
-        echo "E: Invalid command: ${COMMAND}"
-        exit 1
-    ;;
-esac
+    cat ${CONFFILES} >${TEMPFILE}
+    echo "Rules for ${version}: ${TEMPFILE}"
+done
+
