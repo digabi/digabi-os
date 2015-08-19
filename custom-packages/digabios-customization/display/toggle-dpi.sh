468ca53c6d37793458e0c22f1b97104abe556a0c
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-08-19 08:10:20 +0300

    Fix toggling DPI value

--- custom-packages/digabios-customization/display/toggle-dpi.sh
+++ custom-packages/digabios-customization/display/toggle-dpi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+#set -e
 
 DEFAULT="92"
 HIDPI="192"
@@ -11,26 +11,24 @@ XFCONF="/usr/bin/xfconf-query"
 [ ! -x "${XFCONF}" ] && echo "E: xfconf-query not found!" && exit 1
 [ ! -x "${ZENITY}" ] && echo "E: zenity not found!" && exit 1
 
-CURRENT="$(${XFCONF} -c xsettings -p /Xft/DPI)"
+CURRENT="$(${XFCONF} -c xsettings -p /Xft/DPI || echo ${DEFAULT})"
 [ -z "${CURRENT}" ] && CURRENT="${DEFAULT}"
 
-[ "${CURRENT}" = "${DEFAULT}" ] && NEW="${HIDPI}" || NEW="${DEFAULT}"
+if [ "${CURRENT}" = "${DEFAULT}" ]
+then
+    NEW="${HIDPI}"
+else
+    NEW="${DEFAULT}"
+fi
 
+echo "I: Set DPI to ${NEW} (was: ${CURRENT})"
 ${XFCONF} -n -c xsettings -p /Xft/DPI -t int -s ${NEW}
+${ZENITY} --question --text "Do you want to keep the current setting?" --timeout "${TIMEOUT}" 2>/dev/null
 
-zenity --question --timeout ${TIMEOUT} --text "OK?"
-RETVAL="$?"
-
-case "${RETVAL}"
-in
-    0)
-    ;;
-    1)
-    ;;
-    -1)
-    ;;
-    *)
-    ;;
-esac
+if [ $? != 0 ]
+then
+    echo "I: User asked to return previous value..."
+    ${XFCONF} -n -c xsettings -p /Xft/DPI -t int -s ${CURRENT}
+fi
 
 exit 0
