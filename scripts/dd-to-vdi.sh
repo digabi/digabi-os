1085b0909a9db3e74b10de62b6c21e53c217f691
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-08-20 12:08:25 +0300

    Add some error handling

--- scripts/dd-to-vdi.sh
+++ scripts/dd-to-vdi.sh
@@ -2,6 +2,10 @@
 set -e
 
 DDIMAGE="$1"
+
+[ -z "${DDIMAGE}" ] && echo "usage: $0 <path/to/image.dd>" && exit 1
+[ ! -f "${DDIMAGE}" ] && echo "E: File not found: ${DDIMAGE}" && exit 1
+
 VDIIMAGE="$(echo ${DDIMAGE} |sed 's,\.dd$,\.vdi,g')"
 
 VBoxManage convertdd "${DDIMAGE}" "${VDIIMAGE}" --format VDI --variant Fixed
