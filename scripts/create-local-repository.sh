c065cd373d8fda467a9c9c765a47adb5b2d39947
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-09-08 09:22:16 +0300

    Use full path to temporary directory

--- scripts/create-local-repository.sh
+++ scripts/create-local-repository.sh
@@ -6,7 +6,7 @@ SIGNING_KEY="0x9D3D06EE"
 
 CURDIR="${PWD}"
 mkdir -p repos
-TEMPDIR="$(mktemp -d repos/dos-repo.XXXXXXXX)"
+TEMPDIR="$(realpath $(mktemp -d repos/dos-repo.XXXXXXXX))"
 
 echo "I: Creating temporary repository to ${TEMPDIR}..." 1>&2
 cd "${TEMPDIR}"
