93edd43546e3e3c9de944c02c028aa8833fcb04c
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-10-14 15:08:11 +0300

    Show notification after screenshot has been saved

--- custom-packages/digabios/screenshot/save-screenshot-image.sh
+++ custom-packages/digabios/screenshot/save-screenshot-image.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 mv "$1" /tmp/latest_screenshot.png
+notify-send -t 5000 -i applets-screenshooter "Kuvankaappaus otettu" "Palaa sähköiseen kokeeseen ja klikkaa\n'Liitä tekemäsi kuvankaappaus' -painiketta."
