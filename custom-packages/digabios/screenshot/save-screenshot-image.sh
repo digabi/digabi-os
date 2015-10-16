ad9eb3f56d307bc5f2c1a3a4269813fb2154baaf
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-10-16 10:59:13 +0300

    Add svg icon for screenshot, use it in the top panel & notification

--- custom-packages/digabios/screenshot/save-screenshot-image.sh
+++ custom-packages/digabios/screenshot/save-screenshot-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mv "$1" /tmp/latest_screenshot.png
-notify-send -t 5000 -i applets-screenshooter "Kuvankaappaus otettu" "Palaa sähköiseen kokeeseen ja klikkaa\n'Liitä tekemäsi kuvankaappaus' -painiketta."
+notify-send -t 5000 -i digabi-screenshot "Kuvankaappaus otettu" "Palaa sähköiseen kokeeseen ja klikkaa\n'Liitä tekemäsi kuvankaappaus' -painiketta."
