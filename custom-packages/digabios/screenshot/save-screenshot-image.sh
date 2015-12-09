07d3298fcba2e15cc410b0c7d55ba8d0b27e07b4
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-12-09 15:55:38 +0200

    Remove all screenshot related stuff (moved to koe repository)

--- custom-packages/digabios/screenshot/save-screenshot-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mv "$1" /tmp/latest_screenshot.png
-notify-send -t 5000 -i digabi-screenshot "Kuvankaappaus otettu" "Palaa sähköiseen kokeeseen ja klikkaa\n'Liitä tekemäsi kuvankaappaus' -painiketta."
