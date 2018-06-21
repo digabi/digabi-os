73c2e2270350f5d1224c76e34b5cffdab3aa1b8e
Timo Metsälä <timo.metsala@reaktor.com>
2018-06-21 12:39:06 +0300

    Remove more unused files

--- scripts/list-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-RDEPS="/usr/bin/apt-rdepends"
-[ ! -x "${RDEPS}" ] && echo "E: apt-rdepends not installed!" && exit 1
-
-PACKAGES="$*"
-
-${RDEPS} ${PACKAGES} |sed -e 's,  Depends: ,,g' -e 's,  PreDepends: ,,g' |sort -u
