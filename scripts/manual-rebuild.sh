0e379532030bd13b7d1defb7442f0a0af64f7fcf
Perttu Auramo <perttu.auramo@reaktor.com>
2018-08-20 16:07:20 +0300

    Create a rebuild script
    For manual use in a VM

---
+++ scripts/manual-rebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Use this script to manually test the live-build process
+
+set -ex
+
+export DIGABI_BUILD_TARGET="client meb"
+export WLAN=true
+export ROOT_PASSWORD=kukkuu
+export DEBIAN_MIRROR=http://192.168.2.49/debian
+export DEBIAN_SUITE=stretch
+export ARCH=amd64
+
+# Clean up clutter from previous build
+sudo lb clean
+rm sources.list || true
+
+# Fetch the resources
+wget http://ci.local/job/dos-kernel-signing-keys/lastSuccessfulBuild/artifact/run-in-vm-results/signing_key.priv
+wget http://ci.local/job/dos-kernel-signing-keys/lastSuccessfulBuild/artifact/run-in-vm-results/signing_key.x509
+wget http://ci.local/job/dos-repository/lastSuccessfulBuild/artifact/sources.list
+
+sudo rm /etc/apt/sources.list.d/digabi.list || true
+scripts/create-build-config.sh > dos-config
+
+sudo apt remove live-build
+scripts/build-in-vm.sh "digabi-*"
