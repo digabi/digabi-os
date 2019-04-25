8cc4c3be9a3225a4b73703b19279a65cf3261d39
Timo Metsälä <timo.metsala@reaktor.com>
2019-04-25 13:49:46 +0300

    Use the new mirror

--- scripts/manual-rebuild.sh
+++ scripts/manual-rebuild.sh
@@ -7,7 +7,7 @@ set -ex
 export DIGABI_BUILD_TARGET="client meb"
 export WLAN=true
 export ROOT_PASSWORD=kukkuu
-export DEBIAN_MIRROR=http://192.168.2.49/debian
+export DEBIAN_MIRROR=http://192.168.3.32/debian
 export DEBIAN_SUITE=stretch
 export ARCH=amd64
 
@@ -16,9 +16,9 @@ sudo lb clean
 rm sources.list || true
 
 # Fetch the resources
-wget http://ci.local/job/dos-kernel-signing-keys/lastSuccessfulBuild/artifact/run-in-vm-results/signing_key.priv
-wget http://ci.local/job/dos-kernel-signing-keys/lastSuccessfulBuild/artifact/run-in-vm-results/signing_key.x509
-wget http://ci.local/job/dos-repository/lastSuccessfulBuild/artifact/sources.list
+wget http://jenkins.local/job/usb-kernel-signing-keys/lastSuccessfulBuild/artifact/signing_key.priv
+wget http://jenkins.local/job/usb-kernel-signing-keys/lastSuccessfulBuild/artifact/signing_key.x509
+wget http://jenkins.local/job/dos-repository/lastSuccessfulBuild/artifact/sources.list
 
 sudo rm /etc/apt/sources.list.d/digabi.list || true
 scripts/create-build-config.sh > dos-config
