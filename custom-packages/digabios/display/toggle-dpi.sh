966c0a2f4f6e3e0dca765dda223726b3b8fbc2c5
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2022-03-28 15:28:29 +0300

    digabi-dpi: do not set Firefox devPixelsPerPx setting
    It causes the effective zoom level of Firefox to be 200% which causes
    screenshots to be shown at double the size the examinee sees on their
    screen.
    
    (We are also removing screenshot scaling from maybe-scale-screenshot.py)

--- custom-packages/digabios/display/toggle-dpi.sh
+++ custom-packages/digabios/display/toggle-dpi.sh
@@ -8,56 +8,17 @@ TIMEOUT="15"
 ZENITY="/usr/bin/zenity"
 XFCONF="/usr/bin/xfconf-query"
 
-
-close_firefox() {
-    /usr/bin/wmctrl -c Firefox
-    for i in $(seq 10)
-    do
-        pkill -0 firefox-esr || break
-        sleep 0.2
-    done
-
-    pkill -9 firefox-esr 2>&1 >/dev/null
-}
-
-set_scaling_and_start_firefox() {
-    local -r scaling="$1"
-    local -r prefs_file="$(ls ~/.mozilla/firefox/*.default/prefs.js)"
-
-    if grep -q layout.css.devPixelsPerPx "${prefs_file}"
-    then
-        sed -i -e "/layout.css.devPixelsPerPx/s/\"[0-9]*\"/\"${scaling}\"/" "${prefs_file}"
-    else
-        echo "user_pref(\"layout.css.devPixelsPerPx\", \"${scaling}\");" >> "${prefs_file}"
-    fi
-
-    xdg-open /usr/share/applications/digabi-koe-firefox-esr.desktop
-}
-
-wait_for_firefox_to_start() {
-    for i in $(seq 10)
-    do
-        wmctrl -l -x | grep -qi firefox && break
-        sleep 0.2
-    done
-    sleep 0.2
-}
-
 [ ! -x "${XFCONF}" ] && echo "E: xfconf-query not found!" && exit 1
 [ ! -x "${ZENITY}" ] && echo "E: zenity not found!" && exit 1
 
-CURRENT="$(${XFCONF} -c xsettings -p /Xft/DPI || echo ${DEFAULT})"
+CURRENT="$(${XFCONF} -c xsettings -p /Xft/DPI 2>/dev/null || echo ${DEFAULT})"
 [ -z "${CURRENT}" ] && CURRENT="${DEFAULT}"
 
 if [ "${CURRENT}" = "${DEFAULT}" ]
 then
     NEW="${HIDPI}"
-    FF_NEW_SCALING=2
-    FF_OLD_SCALING=1
 else
     NEW="${DEFAULT}"
-    FF_NEW_SCALING=1
-    FF_OLD_SCALING=2
 fi
 
 case "${LANG}"
@@ -80,17 +41,10 @@ esac
 echo "I: Set DPI to ${NEW} (was: ${CURRENT})"
 ${XFCONF} -n -c xsettings -p /Xft/DPI -t int -s ${NEW}
 
-close_firefox
-set_scaling_and_start_firefox ${FF_NEW_SCALING}
-
-wait_for_firefox_to_start
-
 if ! ${ZENITY} --question --text "${QUESTION}" --timeout "${TIMEOUT}" --ok-label "${OK}" --cancel-label "${CANCEL}" 2>/dev/null
 then
     echo "I: User asked to return previous value..."
     ${XFCONF} -n -c xsettings -p /Xft/DPI -t int -s ${CURRENT}
-    close_firefox
-    set_scaling_and_start_firefox ${FF_OLD_SCALING}
 fi
 
 exit 0
