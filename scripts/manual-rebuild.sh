6047168cd1d41760b84bd07fb9c8725235bc60fd
Jarkko Skyttälä <jarkko.skyttala@reaktor.com>
2025-02-12 09:02:40 +0200

    Add way to locally set debian mirror auth without risk of it ending up in repo

--- scripts/manual-rebuild.sh
+++ scripts/manual-rebuild.sh
@@ -4,27 +4,29 @@
 
 set -ex
 
+if [ -f .env ]; then
+  echo "Reading env variables from .env"
+
+  set -a
+  # shellcheck disable=SC1091
+  source .env
+  set +a
+else
+  echo ".env variable not found, initialize it before running"
+fi
+
 export DIGABI_BUILD_TARGET="server meb"
 export INCLUDE_WLAN=true
 export ROOT_PASSWORD=kukkuu
 export DEBIAN_MIRROR="https://debian.abitti.fi/debian-mirror/debian"
-export DEBIAN_MIRROR_AUTH="machine debian.abitti.fi login digabi password password-from-aws-goes-here"
 export DEBIAN_SUITE=bookworm
 export ARCH=amd64
 export DEBIAN_FRONTEND=noninteractive
-
-# Clean up clutter from previous build
-#### NOT NEEDED IN DOCKER
-# sudo lb clean
-# rm sources.list || true
 
 # Fetch the resources
 wget http://jenkins.abitti.fi/job/usb-kernel-signing-keys/lastSuccessfulBuild/artifact/signing_key.priv
 wget http://jenkins.abitti.fi/job/usb-kernel-signing-keys/lastSuccessfulBuild/artifact/signing_key.x509
 wget -O sources.list http://jenkins.abitti.fi/job/dos-repository/lastSuccessfulBuild/artifact/sources.list.bookworm
 
-sudo rm /etc/apt/sources.list.d/digabi.list || true
 scripts/create-build-config.sh > dos-config
-
-# sudo apt remove live-build
 scripts/build-in-vm.sh "digabi-*"
